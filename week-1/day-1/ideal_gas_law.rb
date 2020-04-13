76394933e75220d86cdc0a17b9b3c12bdf107c42
unknown <wilber.carrascal@gmail.com>
2020-04-13 15:15:50 -0500

    ideal_gas_law

---
+++ week-1/day-1/ideal_gas_law.rb
@@ -0,0 +1,6 @@
+def calculate_pressure(chemical_amount, temperature, volume)
+  ideal_gas_constant = 8.314
+  (chemical_amount * ideal_gas_constant * temperature) / volume
+end
+
+puts calculate_pressure(5, 500, 2)
