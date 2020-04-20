feac193fb766b726fae3391a080b69f69d45a795
unknown <wilber.carrascal@gmail.com>
2020-04-20 10:47:05 -0500

    put_n

---
+++ week-2/day-1/put_n.rb
@@ -0,0 +1,12 @@
+def put_n(text, n)
+  raise ArgumentError, 'It has to be a Integer Number' unless n.is_a? Integer
+
+  raise ArgumentError, 'The number have to be Positive' if n.negative?
+
+  n.times { puts text }
+end
+
+# put_n('This will be printed 5 times!', 5.1)
+# put_n('This will be printed 5 times!', 0)
+# put_n('This will be printed 5 times!', -1)
+put_n('This will be printed 5 times!', 5)
