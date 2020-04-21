873f9f0f14c62dabdd3756809a8a2fa99e96ae66
unknown <wilber.carrascal@gmail.com>
2020-04-21 11:31:50 -0500

    fix_map

---
+++ week-2/day-2/fix_map.rb
@@ -0,0 +1,10 @@
+def map(array)
+  array.map { |element| block_given? ? (yield element) : element }
+end
+
+arr = [1, 2, 3]
+p map(arr) { |el| el * 2 } # Returns [2, 4, 6]
+# With no block
+p new_arr = map(arr) # Returns [1, 2, 3]
+p arr.equal?(new_arr) # Returns false, new_arr is a copy of arr. They are not the same object.
+# Note: #equal? checks that two objects are the same instance: https://apidock.com/ruby/Object/equal%3F
